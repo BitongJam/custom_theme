233e035855b2575310a6e183a7d7b85f67a655e5
BitongJam <jamesortizalter@gmail.com>
2024-07-21 21:16:59 +0800

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Custom Theme',
+    'version': '14.0',
+    'description': '''
+        Custom Theme for Practice
+    ''',
+    'summary': '',
+    'author': 'James Michael Ortiz',
+    'depends': [
+        'base','web'
+    ],
+    'data': [
+        'views/assets.xml'
+    ],
+    'auto_install': False,
+    'application': False,
+}
